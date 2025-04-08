e1e0a5f9fd8f44ef9e861060a77fc0fda5f5d304
Thomas134 <1265552377@qq.com>
2025-04-09 00:10:07 +0800

    Refactor shift.cpp

--- src/shift.cpp
+++ src/shift.cpp
@@ -1,5 +1,7 @@
 #include "../include/shift.hpp"
 #include "../include/simd_traits.hpp"
+#include "../include/utils/utils.hpp"
+#include "../include/utils/simd_operators.hpp"
 #include <immintrin.h>
 #include <stdexcept>
 
@@ -7,47 +9,13 @@ namespace internal {
     // slli1_simd
     template <typename T>
     std::vector<T> slli1_simd(const std::vector<T>& A, const int imm8) {
-        if (A.empty())
-            throw std::invalid_argument("Vector can't be empty");
-
-        std::vector<T> result(A.size());
-        size_t i = 0;
-        const size_t simd_step = slli_simd_traits<T>::step;
-
-        for (; i <= A.size() - simd_step; i += simd_step) {
-            auto vec_a = slli_simd_traits<T>::load(&A[i]);
-            auto vec_result = slli_simd_traits<T>::bitwise_slli(vec_a, imm8);
-            slli_simd_traits<T>::store(&result[i], vec_result);
-        }
-
-        for (; i < A.size(); ++i) {
-            result[i] = A[i] << imm8;
-        }
-
-        return result;
+        return apply_unary_op_simd<T, slli_simd_traits<T>>(A, std::min);
     }
 
 
     // srli1_simd
     template <typename T>
     std::vector<T> srli1_simd(const std::vector<T>& A, const int imm8) {
-        if (A.empty())
-            throw std::invalid_argument("Vector can't be empty");
-
-        std::vector<T> result(A.size());
-        size_t i = 0;
-        const size_t simd_step = srli_simd_traits<T>::step;
-
-        for (; i <= A.size() - simd_step; i += simd_step) {
-            auto vec_a = srli_simd_traits<T>::load(&A[i]);
-            auto vec_result = srli_simd_traits<T>::bitwise_srli(vec_a, imm8);
-            srli_simd_traits<T>::store(&result[i], vec_result);
-        }
-
-        for (; i < A.size(); ++i) {
-            result[i] = A[i] >> imm8;
-        }
-
-        return result;
+        return apply_unary_op_simd<T, srli_simd_traits<T>>(A, std::min);
     }
 }
