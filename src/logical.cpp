60ef766f2a89fb025788dcca0c0af1e15e8f9103
Thomas134 <1265552377@qq.com>
2025-04-09 15:58:18 +0800

    Refactor logical.cpp unless testc

--- src/logical.cpp
+++ src/logical.cpp
@@ -1,105 +1,47 @@
 #include "../include/logical.hpp"
 #include "../include/simd_traits.hpp"
+#include "../include/utils/utils.hpp"
+#include "../include/utils/simd_operators.hpp"
 #include <immintrin.h>
 
 namespace internal {
     // and1_simd
     template <typename T>
     std::vector<T> and1_simd(const std::vector<T>& A, const std::vector<T>& B) {
-        if (A.size() != B.size())
-            throw std::invalid_argument("Input vectors must have the same size.");
+        if (A.size() < 32)
+            return apply_binary_op_plain(A, B, [](const T& element1, const T& element2) { return element1 & element2; });
 
-        std::vector<T> result(A.size());
-        const size_t simd_step = and_simd_traits<T>::step;
-        size_t i = 0;
-
-        for (; i <= A.size() - simd_step; i += simd_step) {
-            auto vec_a = and_simd_traits<T>::load(&A[i]);
-            auto vec_b = and_simd_traits<T>::load(&B[i]);
-            auto vec_result = and_simd_traits<T>::bitwise_and(vec_a, vec_b);
-            and_simd_traits<T>::store(&result[i], vec_result);
-        }
-
-        for (; i < A.size(); ++i) {
-            result[i] = A[i] & B[i];
-        }
-
-        return result;
+        return apply_binary_op_simd<T, and_simd_traits<T>>(A, B, [](const T& element1, const T& element2) { return element1 & element2; });
     }
 
 
     // or1_simd
     template <typename T>
     std::vector<T> or1_simd(const std::vector<T>& A, const std::vector<T>& B) {
-        if (A.size() != B.size())
-            throw std::invalid_argument("Input vectors must have the same size.");
+        if (A.size() < 32)
+            return apply_binary_op_plain(A, B, [](const T& element1, const T& element2) { return element1 | element2; });
 
-        std::vector<T> result(A.size());
-        const size_t simd_step = or_simd_traits<T>::step;
-        size_t i = 0;
-
-        for (; i <= A.size() - simd_step; i += simd_step) {
-            auto vec_a = or_simd_traits<T>::load(&A[i]);
-            auto vec_b = or_simd_traits<T>::load(&B[i]);
-            auto vec_result = or_simd_traits<T>::bitwise_or(vec_a, vec_b);
-            or_simd_traits<T>::store(&result[i], vec_result);
-        }
-
-        for (; i < A.size(); ++i) {
-            result[i] = A[i] | B[i];
-        }
-
-        return result;
+        return apply_binary_op_simd<T, or_simd_traits<T>>(A, B, [](const T& element1, const T& element2) { return element1 | element2; });
     }
 
 
     // xor1_simd
     template <typename T>
     std::vector<T> xor1_simd(const std::vector<T>& A, const std::vector<T>& B) {
-        if (A.size() != B.size())
-            throw std::invalid_argument("Input vectors must have the same size.");
+        if (A.size() < 32)
+            return apply_binary_op_plain(A, B, [](const T& element1, const T& element2) { return element1 ^ element2; });
 
-        std::vector<T> result(A.size());
-        size_t i = 0;
-        const size_t simd_step = xor_simd_traits<T>::step;
-
-        for (; i <= A.size() - simd_step; i += simd_step) {
-            auto vec_a = xor_simd_traits<T>::load(&A[i]);
-            auto vec_b = xor_simd_traits<T>::load(&B[i]);
-            auto vec_result = xor_simd_traits<T>::bitwise_xor(vec_a, vec_b);
-            xor_simd_traits<T>::store(&result[i], vec_result);
-        }
-
-        for (; i < A.size(); ++i) {
-            result[i] = A[i] ^ B[i];
-        }
-
-        return result;
+        return apply_binary_op_simd<T, xor_simd_traits<T>>(A, B, [](const T& element1, const T& element2) { return element1 ^ element2; });
     }
 
 
     // andnot1_simd
     template <typename T>
     std::vector<T> andnot1_simd(const std::vector<T>& A, const std::vector<T>& B) {
-        if (A.size() != B.size())
-            throw std::invalid_argument("Input vectors must have the same size.");
+        if (A.size() < 32)
+            return apply_binary_op_plain(A, B, [](const T& element1, const T& element2) { return ~element1 & element2; });
 
-        std::vector<T> result(A.size());
-        size_t i = 0;
-        const size_t simd_step = andnot_simd_traits<T>::step;
-
-        for (; i <= A.size() - simd_step; i += simd_step) {
-            auto vec_a = andnot_simd_traits<T>::load(&A[i]);
-            auto vec_b = andnot_simd_traits<T>::load(&B[i]);
-            auto vec_result = andnot_simd_traits<T>::bitwise_andnot(vec_a, vec_b);
-            andnot_simd_traits<T>::store(&result[i], vec_result);
-        }
-
-        for (; i < A.size(); ++i) {
-            result[i] = ~A[i] & B[i];
-        }
-
-        return result;
+        return apply_binary_op_simd<T, andnot_simd_traits<T>>(A, B, [](const T& element1, const T& element2) { return ~element1 & element2; });
     }
 
 
