5d04b00c8102ca600c706ac803b938323f9d1a96
Thomas134 <1265552377@qq.com>
2025-04-12 17:03:29 +0800

    Add extra math simd functions

--- include/math.hpp
+++ include/math.hpp
@@ -1,15 +1,14 @@
 #ifndef MATH_HPP
 #define MATH_HPP
 
-#include "simd_traits.hpp"
 #include <vector>
 
 namespace internal {
     // ================================= 1D ====================================
 
     // inner_product_simd
-    template <typename T>
-    typename inner_product_simd_traits<T>::accum_type inner_product_simd(const std::vector<T>& A, const std::vector<T>& B);
+    // template <typename T>
+    // typename inner_product_simd_traits<T>::accum_type inner_product_simd(const std::vector<T>& A, const std::vector<T>& B);
 
 
     // min1
@@ -50,6 +49,56 @@ namespace internal {
     // abs1
     template <typename T>
     std::vector<T> abs1_simd(const std::vector<T>& A);
+
+
+    // log_1
+    template <typename T>
+    std::vector<T> log_1_simd(const std::vector<T>& A);
+
+
+    // log2_1
+    template <typename T>
+    std::vector<T> log2_1_simd(const std::vector<T>& A);
+
+
+    // log10_1
+    template <typename T>
+    std::vector<T> log10_1_simd(const std::vector<T>& A);
+
+
+    // sin1
+    template <typename T>
+    std::vector<T> sin1_simd(const std::vector<T>& A);
+
+
+    // cos1
+    template <typename T>
+    std::vector<T> cos1_simd(const std::vector<T>& A);
+
+
+    // sincos1
+    template <typename T>
+    std::vector<T> sincos1_simd(const std::vector<T>& A);
+
+
+    // tan1
+    template <typename T>
+    std::vector<T> tan1_simd(const std::vector<T>& A);
+
+
+    // asin1
+    template <typename T>
+    std::vector<T> asin1_simd(const std::vector<T>& A);
+
+
+    // acos1
+    template <typename T>
+    std::vector<T> acos1_simd(const std::vector<T>& A);
+
+
+    // atan1
+    template <typename T>
+    std::vector<T> atan1_simd(const std::vector<T>& A);
 
 
     // ================================= 2D ====================================
@@ -94,6 +143,56 @@ namespace internal {
     // abs2
     template <typename T>
     std::vector<std::vector<T>> abs2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // log_2
+    template <typename T>
+    std::vector<std::vector<T>> log_2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // log2_2
+    template <typename T>
+    std::vector<std::vector<T>> log2_2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // log10_2
+    template <typename T>
+    std::vector<std::vector<T>> log10_2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // sin2
+    template <typename T>
+    std::vector<std::vector<T>> sin2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // cos2
+    template <typename T>
+    std::vector<std::vector<T>> cos2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // sincos2
+    template <typename T>
+    std::vector<std::vector<T>> sincos2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // tan2
+    template <typename T>
+    std::vector<std::vector<T>> tan2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // asin2
+    template <typename T>
+    std::vector<std::vector<T>> asin2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // acos2
+    template <typename T>
+    std::vector<std::vector<T>> acos2_simd(const std::vector<std::vector<T>>& A);
+
+
+    // atan2
+    template <typename T>
+    std::vector<std::vector<T>> atan2_simd(const std::vector<std::vector<T>>& A);
 }
 
 #endif
--- src/math.cpp
+++ src/math.cpp
@@ -1,4 +1,6 @@
 #include "../include/math.hpp"
+#include "../include/simd_traits.hpp"
+#include "../include/xsimd_traits.hpp"
 #include "../include/utils/utils.hpp"
 #include "../include/utils/simd_operators.hpp"
 #include <type_traits>
@@ -6,38 +8,38 @@
 
 
 namespace internal {
-    template <typename T>
-    typename inner_product_simd_traits<T>::accum_type inner_product_simd(const std::vector<T>& A, const std::vector<T>& B) {
-        if (A.size() != B.size()) {
-            throw std::invalid_argument("Input vectors must have the same size");
-        }
+    // template <typename T>
+    // typename inner_product_simd_traits<T>::accum_type inner_product_simd(const std::vector<T>& A, const std::vector<T>& B) {
+    //     if (A.size() != B.size()) {
+    //         throw std::invalid_argument("Input vectors must have the same size");
+    //     }
 
-        using Traits = inner_product_simd_traits<T>;
-        using simd_type = typename Traits::simd_type;
-        using accum_type = typename Traits::accum_type;
+    //     using Traits = inner_product_simd_traits<T>;
+    //     using simd_type = typename Traits::simd_type;
+    //     using accum_type = typename Traits::accum_type;
 
-        accum_type result = static_cast<accum_type>(0);
-        simd_type sum = Traits::zero();
+    //     accum_type result = static_cast<accum_type>(0);
+    //     simd_type sum = Traits::zero();
 
-        const size_t n = A.size();
-        const T *ptr_A = A.data();
-        const T *ptr_B = B.data();
+    //     const size_t n = A.size();
+    //     const T *ptr_A = A.data();
+    //     const T *ptr_B = B.data();
 
-        size_t i = 0;
-        for (; i <= n - Traits::step; i += Traits::step) {
-            simd_type va = Traits::load(ptr_A + i);
-            simd_type vb = Traits::load(ptr_B + i);
-            sum = Traits::mul_add(va, vb, sum);
-        }
+    //     size_t i = 0;
+    //     for (; i <= n - Traits::step; i += Traits::step) {
+    //         simd_type va = Traits::load(ptr_A + i);
+    //         simd_type vb = Traits::load(ptr_B + i);
+    //         sum = Traits::mul_add(va, vb, sum);
+    //     }
 
-        result = Traits::horizontal_sum(sum);
+    //     result = Traits::horizontal_sum(sum);
 
-        for (; i < n; ++i) {
-            result += ptr_A[i] * ptr_B[i];
-        }
+    //     for (; i < n; ++i) {
+    //         result += ptr_A[i] * ptr_B[i];
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
 
     // min1_simd
@@ -130,6 +132,99 @@ namespace internal {
     }
 
 
+    // log_1_simd
+    template <typename T>
+    std::vector<T> log_1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::log);
+
+        return apply_unary_op_simd<T, log_simd_traits<T>>(A, std::log);
+    }
+
+
+    // log2_1_simd
+    template <typename T>
+    std::vector<T> log2_1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::log2);
+
+        return apply_unary_op_simd<T, log2_simd_traits<T>>(A, std::log2);
+    }
+
+
+    // log10_1_simd
+    template <typename T>
+    std::vector<T> log10_1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::log10);
+
+        return apply_unary_op_simd<T, log10_simd_traits<T>>(A, std::log10);
+    }
+
+
+    // sin1_simd
+    template <typename T>
+    std::vector<T> sin1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::sin);
+
+        return apply_unary_op_simd<T, sin_simd_traits<T>>(A, std::sin);
+    }
+
+
+    // cos1_simd
+    template <typename T>
+    std::vector<T> cos1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::cos);
+
+        return apply_unary_op_simd<T, cos_simd_traits<T>>(A, std::cos);
+    }
+
+
+    // sincos1_simd
+    
+
+    // tan1_simd
+    template <typename T>
+    std::vector<T> tan1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::tan);
+
+        return apply_unary_op_simd<T, tan_simd_traits<T>>(A, std::tan);
+    }
+
+
+    // asin1_simd
+    template <typename T>
+    std::vector<T> asin1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::asin);
+
+        return apply_unary_op_simd<T, asin_simd_traits<T>>(A, std::asin);
+    }
+
+
+    // acos1_simd
+    template <typename T>
+    std::vector<T> acos1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::acos);
+
+        return apply_unary_op_simd<T, acos_simd_traits<T>>(A, std::acos);
+    }
+
+
+    // atan1_simd
+    template <typename T>
+    std::vector<T> atan1_simd(const std::vector<T>& A) {
+        if (A.size() < 32)
+            return apply_unary_op_plain(A, std::atan);
+
+        return apply_unary_op_simd<T, atan_simd_traits<T>>(A, std::atan);
+    }
+
+
     // ================================= 2D ====================================
 
     // min2_simd
@@ -187,5 +282,68 @@ namespace internal {
     template <typename T>
     std::vector<std::vector<T>> abs2_simd(const std::vector<std::vector<T>>& A) {
         return apply_unary_op(A, abs1_simd);
+    }
+
+
+    // log_2_simd
+    template <typename T>
+    std::vector<std::vector<T>> log_2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, log_1_simd);
+    }
+
+
+    // log2_2_simd
+    template <typename T>
+    std::vector<std::vector<T>> log2_2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, log2_1_simd);
+    }
+
+
+    // log10_2_simd
+    template <typename T>
+    std::vector<std::vector<T>> log10_2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, log10_1_simd);
+    }
+
+
+    // sin2_simd
+    template <typename T>
+    std::vector<std::vector<T>> sin2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, sin1_simd);
+    }
+
+
+    // cos2_simd
+    template <typename T>
+    std::vector<std::vector<T>> cos2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, cos1_simd);
+    }
+
+
+    // tan2_simd
+    template <typename T>
+    std::vector<std::vector<T>> tan2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, tan1_simd);
+    }
+
+
+    // asin2_simd
+    template <typename T>
+    std::vector<std::vector<T>> asin2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, asin1_simd);
+    }
+
+
+    // acos2_simd
+    template <typename T>
+    std::vector<std::vector<T>> acos2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, acos1_simd);
+    }
+
+
+    // atan2_simd
+    template <typename T>
+    std::vector<std::vector<T>> atan2_simd(const std::vector<std::vector<T>>& A) {
+        return apply_unary_op(A, atan1_simd);
     }
 }
