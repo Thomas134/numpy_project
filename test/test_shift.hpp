1ee145c6f71173293e962804bc8dec1000fd3e6d
Thomas134 <1265552377@qq.com>
2025-04-21 22:50:42 +0800

    Change test_shift to random test

--- test/test_shift.hpp
+++ test/test_shift.hpp
@@ -1,10 +1,21 @@
 #include <gtest/gtest.h>
 #include "../include/data_structure/ndarray.cpp"
+#include <random>
+
+int generateRandomInt(int min, int max) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(min, max);
+    return dis(gen);
+}
 
 TEST(NDArrayShiftTest, Slli1DTest) {
-    std::vector<size_t> shape = {4};
+    std::vector<size_t> shape = {1000};
     ndarray<int> arr(shape);
-    std::vector<int> data = {1, 2, 3, 4};
+    std::vector<int> data;
+    for (size_t i = 0; i < shape[0]; ++i) {
+        data.push_back(generateRandomInt(1, 10));
+    }
     arr.assign(data);
 
     constexpr int8_t imm = 2;
@@ -18,9 +29,12 @@ TEST(NDArrayShiftTest, Slli1DTest) {
 }
 
 TEST(NDArrayShiftTest, Srli1DTest) {
-    std::vector<size_t> shape = {4};
+    std::vector<size_t> shape = {1000};
     ndarray<int> arr(shape);
-    std::vector<int> data = {4, 8, 12, 16};
+    std::vector<int> data;
+    for (size_t i = 0; i < shape[0]; ++i) {
+        data.push_back(generateRandomInt(4, 20));
+    }
     arr.assign(data);
 
     constexpr int8_t imm = 2;
@@ -34,9 +48,14 @@ TEST(NDArrayShiftTest, Srli1DTest) {
 }
 
 TEST(NDArrayShiftTest, Slli2DTest) {
-    std::vector<size_t> shape = {2, 2};
+    std::vector<size_t> shape = {1000, 1000};
     ndarray<int> arr(shape);
-    std::vector<std::vector<int>> data = {{1, 2}, {3, 4}};
+    std::vector<std::vector<int>> data(shape[0], std::vector<int>(shape[1]));
+    for (size_t i = 0; i < shape[0]; ++i) {
+        for (size_t j = 0; j < shape[1]; ++j) {
+            data[i][j] = generateRandomInt(1, 10);
+        }
+    }
     arr.assign(data);
 
     constexpr int8_t imm = 2;
@@ -53,9 +72,14 @@ TEST(NDArrayShiftTest, Slli2DTest) {
 }
 
 TEST(NDArrayShiftTest, Srli2DTest) {
-    std::vector<size_t> shape = {2, 2};
+    std::vector<size_t> shape = {1000, 1000};
     ndarray<int> arr(shape);
-    std::vector<std::vector<int>> data = {{4, 8}, {12, 16}};
+    std::vector<std::vector<int>> data(shape[0], std::vector<int>(shape[1]));
+    for (size_t i = 0; i < shape[0]; ++i) {
+        for (size_t j = 0; j < shape[1]; ++j) {
+            data[i][j] = generateRandomInt(4, 20);
+        }
+    }
     arr.assign(data);
 
     constexpr int8_t imm = 2;
