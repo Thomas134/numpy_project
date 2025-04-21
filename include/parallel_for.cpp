d44adb3ca011e59bc33995553c92ea41e2c8a088
Thomas134 <1265552377@qq.com>
2025-04-21 22:21:38 +0800

    Add cmake buildings for test directory

--- include/parallel_for.cpp
+++ include/parallel_for.cpp
@@ -30,7 +30,7 @@ namespace internal {
     // apply2
     template <typename T, typename Func>
     void apply2(std::vector<std::vector<T>>& A, Func func) {
-        #pragma omp parallel for collapse(2)
+        #pragma omp parallel for
         for (size_t i = 0; i < A.size(); ++i) {
             for (size_t j = 0; j < A[i].size(); ++j) {
                 A[i][j] = func(A[i][j]);
